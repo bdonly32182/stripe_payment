55843929e2bbd0210ed654b2876eda59bf8e15ce
jakkaphan piaphengton <jakkaphanpiaphengton@jakkaphansMBP2.lan>
2024-01-03 09:11:36 +0700

    feat: stripe checkout session

---
+++ index.js
@@ -0,0 +1,45 @@
+const Stripe = require("stripe");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+const stripe = Stripe(process.env.STRIPE);
+app.use(cors());
+app.use(express.json());
+app.post("/create-checkout-session", async (req, res) => {
+  const { orders, billId } = req.body;
+  const lineItems = orders.map((room) => {
+    return {
+      price_data: {
+        currency: "thb",
+        unit_amount: room.price * 100,
+        product_data: {
+          name: room?.roomName || "unknow",
+          // images: [room.coverImage],
+          metadata: {
+            id: room.id,
+          },
+        },
+      },
+      quantity: room.totalRoom,
+    };
+  });
+  const session = await stripe.checkout.sessions.create({
+    mode: "payment",
+    shipping_address_collection: {
+      allowed_countries: ["TH"],
+    },
+    phone_number_collection: {
+      enabled: true,
+    },
+    success_url: `${process.env.CLIENT_URL}/success/${billId}`,
+    cancel_url: `${process.env.CLIENT_URL}/cart`,
+    line_items: lineItems,
+  });
+  res.send({ url: session.url, sessionId: session.id });
+});
+
+app.listen(3000, () => {
+  console.log("Running on port 3000");
+});
